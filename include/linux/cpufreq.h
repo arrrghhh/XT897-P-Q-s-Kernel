824282e9fb2c8d6f257528b6ae70ee2ade40ad8c
arrrghhh <arrrghhh@xdandroid.com>
2013-05-11 00:54:32 -0600

    Added lagfree governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -369,6 +369,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lagfree)
 #endif
 
 
